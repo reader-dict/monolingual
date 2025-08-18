8fdb033f5290a5e91c358e0e5b5a6092027c8670
Mickaël Schoentgen <contact@tiger-222.fr>
2025-08-18 15:26:43 +0200

    chore: clean-up after 6db9323d87f6851c38ffe22e98843e102fb3f5c7

--- wikidict/utils.py
+++ wikidict/utils.py
@@ -891,11 +891,9 @@ def render_formula(formula: str, *, cat: str = "tex", output_format: str = "svg"
     if cat == "chem":
         formula = f"\\ce{{{formula}}}"
 
-    headers = constants.WIKIMEDIA_HEADERS
-
     # 1. Get the formula hash (type can be tex, inline-tex, or chem)
     url_hash = constants.WIKIMEDIA_URL_MATH_CHECK.format(type=cat)
-    with constants.SESSION.post(url_hash, headers=headers, json={"q": formula}) as req:
+    with constants.SESSION.post(url_hash, json={"q": formula}) as req:
         req.raise_for_status()
         res = req.json()
         assert res["success"]
@@ -903,7 +901,7 @@ def render_formula(formula: str, *, cat: str = "tex", output_format: str = "svg"
 
     # 2. Get the rendered formula (format can be svg, mml, or png)
     url_render = constants.WIKIMEDIA_URL_MATH_RENDER.format(format=output_format, hash=formula_hash)
-    with constants.SESSION.get(url_render, headers=headers) as req:
+    with constants.SESSION.get(url_render) as req:
         req.raise_for_status()
         return req.text
 
