e798568763d879354cddb64e2919c89fe613feb2
Mickaël Schoentgen <contact@tiger-222.fr>
2025-09-27 10:04:59 +0200

    fix: "Wikidata entity link" support
    Related to #2528.

--- wikidict/lang/defaults.py
+++ wikidict/lang/defaults.py
@@ -137,10 +137,12 @@ def render_wikidata_entity_link(tpl: str, parts: list[str], data: defaultdict[st
     """
     >>> render_wikidata_entity_link("Wikidata entity link", ["112383134"], defaultdict(str))
     'Steve Bruce'
+    >>> render_wikidata_entity_link("Wikidata entity link", ["Q112383134"], defaultdict(str))
+    'Steve Bruce'
     """
     from .en import wikidata
 
-    return wikidata.person(f"Q{parts[0]}", name_only=True)
+    return wikidata.person(f"Q{parts[0].lstrip('Q')}", name_only=True)
 
 
 def render_wikilink(tpl: str, parts: list[str], data: defaultdict[str, str], *, word: str = "") -> str:
