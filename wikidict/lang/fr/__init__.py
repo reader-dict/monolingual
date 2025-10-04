04ff7e81b216a09fe455998366e2599434fbe1d0
Mickaël Schoentgen <contact@tiger-222.fr>
2025-10-04 15:50:06 +0200

    [FR] template adapters

--- wikidict/lang/fr/__init__.py
+++ wikidict/lang/fr/__init__.py
@@ -924,3 +924,10 @@ def adjust_wikicode(
         lines.append(line)
 
     return "\n".join(lines)
+
+
+# Wiktionary modules/templates to alter in the database directly
+template_adapters = {
+    "Modèle:radical de Kangxi": lambda _: "Radical de Kangxi {{numéro|{{#expr: {{point de code|{{PAGENAME}}|format=%d}} - 12032 + 1}}}} [[{{str left|{{radical trait|{{PAGENAME}}}}|1}}]]. Unicode : U+{{point de code|{{PAGENAME}}}}.",
+    "Modèle:variante du radical de Kangxi": lambda _: "Variante {{{1|}}} du radical de Kangxi [[{{str left|{{radical trait|{{PAGENAME}}}}|1}}]]. Unicode : U+{{point de code|{{PAGENAME}}}}.",
+}
