7c0eb1a4ecf43626bb4fd6580f2a8d3cce39b2df
Mickaël Schoentgen <contact@tiger-222.fr>
2025-10-11 04:36:57 +0200

    [FR] Remove category handling from several templates
    Related to #2545.

--- wikidict/lang/fr/template_adapters.py
+++ wikidict/lang/fr/template_adapters.py
@@ -1,4 +1,14 @@
+import re
+
 adapters = {
+    **dict.fromkeys(
+        {
+            "Modèle:emploi",
+            "Modèle:lexique",
+            "Modèle:term",
+        },
+        lambda body: re.sub(r"\[\[Catégorie:[^\]]+\]\]", "", body),
+    ),
     "Modèle:radical de Kangxi": lambda _: "Radical de Kangxi {{numéro|{{#expr: {{point de code|{{PAGENAME}}|format=%d}} - 12032 + 1}}}} [[{{str left|{{radical trait|{{PAGENAME}}}}|1}}]]. Unicode : U+{{point de code|{{PAGENAME}}}}.",
     "Modèle:variante du radical de Kangxi": lambda _: "Variante {{{1|}}} du radical de Kangxi [[{{str left|{{radical trait|{{PAGENAME}}}}|1}}]]. Unicode : U+{{point de code|{{PAGENAME}}}}.",
 }
