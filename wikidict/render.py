260d21a2570218974d4d2f8b5250bfe175d88900
Mickaël Schoentgen <contact@tiger-222.fr>
2025-10-13 11:03:13 +0200

    feat: handle reverse variants only for supported locales

--- wikidict/render.py
+++ wikidict/render.py
@@ -627,15 +627,18 @@ def render(in_words: dict[str, str], locale: str, workers: int) -> Words:
 
     utils.check_for_templates_status(templates_status._getvalue())
 
-    log.info("Handling reverse variants ...")
     results_final: Words = results._getvalue()
-    for word, details in results.items():
-        for form in details.reverse_variants:
-            try:
-                results_final[form].variants = sorted({*results_final[form].variants, word})
-            except KeyError:
-                results_final[form] = Word([], [], [], {}, [word], [])
-    log.info("Handling reverse variants ... Done")
+
+    _, lang_dst = utils.guess_locales(locale, use_log=False)
+    if lang.reverse_variant_titles[lang_dst]:
+        log.info("Handling reverse variants ...")
+        for word, details in results.items():
+            for form in details.reverse_variants:
+                try:
+                    results_final[form].variants = sorted({*results_final[form].variants, word})
+                except KeyError:
+                    results_final[form] = Word([], [], [], {}, [word], [])
+        log.info("Handling reverse variants ... Done")
 
     return results_final
 
