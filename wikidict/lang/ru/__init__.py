1ca6f5f259d585748f50013388018387d609026f
Mickaël Schoentgen <contact@tiger-222.fr>
2025-10-16 10:14:02 +0200

    [RU] Prevent more false variant issue
    Example: https://ru.wiktionary.org/wiki/%D0%90%D0%B4%D1%80%D0%B8%D0%B0%D0%BD%D0%BE%D0%B2%D0%B8%D1%87

--- wikidict/lang/ru/__init__.py
+++ wikidict/lang/ru/__init__.py
@@ -99,17 +99,21 @@ def adjust_wikicode(
     word: str = "",
 ) -> str:
     # sourcery skip: inline-immediately-returned-variable
-    """
+    r"""
     >>> from ... import context
     >>> _ = context.reset("ru")
     >>> context.new_word("word")
 
-    >>> adjust_wikicode("= {{-ru-}} =\\n{{сущ ru m a 2b|основа=коро́л|основа1=корол}}", "ru")
-    '= {{-ru-}} =\\n{{сущ ru m a 2b|}}\\n# {{rev-flexion|коро́ль}}\\n# {{rev-flexion|короле́}}\\n# {{rev-flexion|короле́й}}\\n# {{rev-flexion|короли́}}\\n# {{rev-flexion|королю́}}\\n# {{rev-flexion|короля́}}\\n# {{rev-flexion|короля́м}}\\n# {{rev-flexion|короля́ми}}\\n# {{rev-flexion|короля́х}}\\n# {{rev-flexion|королём}}'
-    >>> adjust_wikicode("= {{-ru-}} =\\n{{сущ ru m a 2b\\n|основа=коро́л\\n|основа1=корол\\n|слоги={{по-слогам|ко|ро́ль}}\\n}}", "ru")
-    '= {{-ru-}} =\\n{{сущ ru m a 2b|}}\\n# {{rev-flexion|коро́ль}}\\n# {{rev-flexion|короле́}}\\n# {{rev-flexion|короле́й}}\\n# {{rev-flexion|короли́}}\\n# {{rev-flexion|королю́}}\\n# {{rev-flexion|короля́}}\\n# {{rev-flexion|короля́м}}\\n# {{rev-flexion|короля́ми}}\\n# {{rev-flexion|короля́х}}\\n# {{rev-flexion|королём}}'
-    >>> adjust_wikicode("= {{-ru-}} =\\n{{прил ru 1*a\\n|основа=бессу́дорожн\\n|основа1=\\n|тип=\\n|слоги={{по-слогам|бес|су́|до|рож|ный}}\\n|степень=\\n|краткая=\\n|коммент=\\n|дореф=\\n}}\\n\\n{{слобр|ru|судорожный|{{выдел|бес}} + судорожный|п|и=}}\\n{{морфо-ru|бес-|судорож|-н|+ый}}", "ru")
-    '= {{-ru-}} =\\n{{прил ru 1*a|}}\\n# {{rev-flexion|бессу́дорожна}}\\n# {{rev-flexion|бессу́дорожная}}\\n# {{rev-flexion|бессу́дорожно}}\\n# {{rev-flexion|бессу́дорожного}}\\n# {{rev-flexion|бессу́дорожное}}\\n# {{rev-flexion|бессу́дорожной}}\\n# {{rev-flexion|бессу́дорожном}}\\n# {{rev-flexion|бессу́дорожному}}\\n# {{rev-flexion|бессу́дорожною}}\\n# {{rev-flexion|бессу́дорожную}}\\n# {{rev-flexion|бессу́дорожны}}\\n# {{rev-flexion|бессу́дорожные}}\\n# {{rev-flexion|бессу́дорожный}}\\n# {{rev-flexion|бессу́дорожным}}\\n# {{rev-flexion|бессу́дорожными}}\\n# {{rev-flexion|бессу́дорожных}}\\n\\n{{слобр|ru|судорожный|{{выдел|бес}} + судорожный|п|и=}}\\n{{морфо-ru|бес-|судорож|-н|+ый}}'
+    >>> adjust_wikicode("= {{-ru-}} =\n{{сущ ru m a 2b|основа=коро́л|основа1=корол}}", "ru")
+    '= {{-ru-}} =\n{{сущ ru m a 2b|}}\n# {{rev-flexion|коро́ль}}\n# {{rev-flexion|короле́}}\n# {{rev-flexion|короле́й}}\n# {{rev-flexion|короли́}}\n# {{rev-flexion|королю́}}\n# {{rev-flexion|короля́}}\n# {{rev-flexion|короля́м}}\n# {{rev-flexion|короля́ми}}\n# {{rev-flexion|короля́х}}\n# {{rev-flexion|королём}}'
+    >>> adjust_wikicode("= {{-ru-}} =\n{{сущ ru m a 2b\n|основа=коро́л\n|основа1=корол\n|слоги={{по-слогам|ко|ро́ль}}\n}}", "ru")
+    '= {{-ru-}} =\n{{сущ ru m a 2b|}}\n# {{rev-flexion|коро́ль}}\n# {{rev-flexion|короле́}}\n# {{rev-flexion|короле́й}}\n# {{rev-flexion|короли́}}\n# {{rev-flexion|королю́}}\n# {{rev-flexion|короля́}}\n# {{rev-flexion|короля́м}}\n# {{rev-flexion|короля́ми}}\n# {{rev-flexion|короля́х}}\n# {{rev-flexion|королём}}'
+    >>> adjust_wikicode("= {{-ru-}} =\n{{прил ru 1*a\n|основа=бессу́дорожн\n|основа1=\n|тип=\n|слоги={{по-слогам|бес|су́|до|рож|ный}}\n|степень=\n|краткая=\n|коммент=\n|дореф=\n}}\n\n{{слобр|ru|судорожный|{{выдел|бес}} + судорожный|п|и=}}\n{{морфо-ru|бес-|судорож|-н|+ый}}", "ru")
+    '= {{-ru-}} =\n{{прил ru 1*a|}}\n# {{rev-flexion|бессу́дорожна}}\n# {{rev-flexion|бессу́дорожная}}\n# {{rev-flexion|бессу́дорожно}}\n# {{rev-flexion|бессу́дорожного}}\n# {{rev-flexion|бессу́дорожное}}\n# {{rev-flexion|бессу́дорожной}}\n# {{rev-flexion|бессу́дорожном}}\n# {{rev-flexion|бессу́дорожному}}\n# {{rev-flexion|бессу́дорожною}}\n# {{rev-flexion|бессу́дорожную}}\n# {{rev-flexion|бессу́дорожны}}\n# {{rev-flexion|бессу́дорожные}}\n# {{rev-flexion|бессу́дорожный}}\n# {{rev-flexion|бессу́дорожным}}\n# {{rev-flexion|бессу́дорожными}}\n# {{rev-flexion|бессу́дорожных}}\n\n{{слобр|ru|судорожный|{{выдел|бес}} + судорожный|п|и=}}\n{{морфо-ru|бес-|судорож|-н|+ый}}'
+
+    >>> context.new_word("Адрианович")
+    >>> adjust_wikicode("= {{-ru-}} =\n{{сущ ru m a 4a\n|основа={{PAGENAME}}\n|основа1={{PAGENAME}}\n|слоги={{по-слогам|Ад|ри|.|а́|.|но|вич}}\n}} {{собств.|ru|тип=отчество}}", "ru")
+    '= {{-ru-}} =\n{{сущ ru m a 4a|}}\n# {{rev-flexion|Адрианович}}\n# {{rev-flexion|Адриановича}}\n# {{rev-flexion|Адриановичам}}\n# {{rev-flexion|Адриановичами}}\n# {{rev-flexion|Адриановичах}}\n# {{rev-flexion|Адриановиче}}\n# {{rev-flexion|Адриановичей}}\n# {{rev-flexion|Адриановичем}}\n# {{rev-flexion|Адриановичи}}\n# {{rev-flexion|Адриановичу}}'
     """
 
     #
@@ -142,11 +146,17 @@ def adjust_wikicode(
                 tpl_code += line
                 if tpl_code.count("{") == tpl_code.count("}"):
                     in_tpl = False
-                    tpl_code, rest = tpl_code.rsplit("}}", 1)
-                    if not rest:
-                        tpl_code += "}}"
+                    tpl_code = tpl_code.rsplit("}}", 1)[0]
+                    tpl_code += "}}"
                     tpl_name = tpl_code[2 : max(0, tpl_code.find("|")) or tpl_code.find("}")].strip()
                     variant_handlers_mod.append_to_reverse_variants(tpl_name)
+
+                    # Remove unrelated templates after a reverse variant one
+                    # `{{сущ ru m a 4a|...}} {{собств.|ru|тип=отчество}}` → `{{сущ ru m a 4a|...}}`
+                    tpl_code_2 = re.split(r"}}\s*\{\{", tpl_code, maxsplit=1)[0]
+                    if tpl_code != tpl_code_2:
+                        tpl_code = tpl_code_2 + "}}"
+
                     forms = utils.process_templates(
                         word,
                         tpl_code,
@@ -156,8 +166,6 @@ def adjust_wikicode(
                     )
                     cleaned.append(f"{{{{{tpl_name}|}}}}")  # This is required for genders finding
                     cleaned.extend(f"# {{{{rev-flexion|{form}}}}}" for form in sorted(forms.split("|")))
-                    if rest:
-                        cleaned.append(rest)
                     tpl_code = ""
             else:
                 cleaned.append(line)
