e2b96a839377360062d94f3face8c1162e7b5962
Mickaël Schoentgen <contact@tiger-222.fr>
2026-01-10 00:38:38 +0100

    chore: update date

--- wikidict/__init__.py
+++ wikidict/__init__.py
@@ -5,10 +5,10 @@ You can always get the latest version of this module at:
     https://github.com/reader-dict/monolingual
 """
 
-__version__ = "5.0.0"
+__version__ = "6.0.0"
 __author__ = "Mickaël Schoentgen"
 __copyright__ = f"""
-Copyright (c) 2020-2025, {__author__}
+Copyright (c) 2020-2026, {__author__}
 
 Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose and without fee or royalty is hereby
