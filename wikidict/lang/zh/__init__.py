efb44a35caa4125acac2f6db40d5167c971825cd
Mickaël Schoentgen <contact@tiger-222.fr>
2026-02-17 11:24:50 +0100

    [ZH] Remove "*" section pattern

--- wikidict/lang/zh/__init__.py
+++ wikidict/lang/zh/__init__.py
@@ -7,7 +7,7 @@ random_word_url = "https://zh.wiktionary.org/wiki/Special:RandomRootpage"
 float_separator = ","
 thousands_separator = ","
 
-section_patterns = ("#", r"\*", ":")
+section_patterns = ("#", ":")
 head_sections = ("漢語", "汉语", "{{漢}}")
 etyl_section = ("词源", "詞源")
 sections = (
