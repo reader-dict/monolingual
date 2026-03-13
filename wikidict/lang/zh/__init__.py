faad60dcd0a352184c85913b4191061d4b645dae
Mickaël Schoentgen <contact@tiger-222.fr>
2026-03-13 19:27:26 +0100

    [ZH] Fix

--- wikidict/lang/zh/__init__.py
+++ wikidict/lang/zh/__init__.py
@@ -161,7 +161,7 @@ def find_pronunciations(code: str, locale: str) -> list[str]:
 
 
 # Example: 興{xīng}
-HAN_FOLLOWED_BY_BRACKETS = regex.compile(r"(?<=\p{Han})(\{[^}]+\})")
+HAN_FOLLOWED_BY_BRACKETS = regex.compile(r"(?<=\p{Han})(\{[^{}]+\})")
 
 
 def adjust_wikicode(
@@ -178,6 +178,11 @@ def adjust_wikicode(
 
     >>> adjust_wikicode("==漢語==\n{{trans-top|...}}\n...\n{{trans-bottom}}", "zh")
     '==漢語==\n'
+
+    >>> adjust_wikicode("興{xīng}", "zh")
+    '興'
+    >>> adjust_wikicode("月亮是眾生不停輪迴轉世的象徵{{...}}所以用“月亮”這", "zh")
+    '月亮是眾生不停輪迴轉世的象徵{{...}}所以用“月亮”這'
 
     >> adjust_wikicode("==漢語==\n; '''限定代詞'''", "zh")
     '==漢語==\n'
